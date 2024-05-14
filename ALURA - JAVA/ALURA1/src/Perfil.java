00421c3817bd5a4da4d9c8e3a48e2a97b257c7f9
Samuel <samuka.samuca321@gmail.com>
2024-05-14 20:20:33 -0300

    Delete ALURA - JAVA/ALURA1 directory

--- ALURA - JAVA/ALURA1/src/Perfil.java
+++
@@ -1,6 +0,0 @@
-public class Perfil {
-    public static void main(String[] args) {
-        System.out.println("Olá meu nome é Samuel!");
-        System.out.println("Tudo bem?");
-    }
-}
--- ALURA - JAVA/ALURA1/src/subtracao.java
+++
@@ -1,6 +0,0 @@
-public class subtracao {
-    public static void main(String[] args) {
-        int x = 10 - 5;
-        System.out.println(x);
-    }
-}
